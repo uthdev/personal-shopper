5c7a967e4154681217ba61a4eab61c2e4f56a6b3
uthdev <adelekegbolahan92@yahoo.com>
2026-02-04 18:26:03 +0100

    feat: implement automatic database seeding with init containers

--- product-service/src/database.ts
+++ product-service/src/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { config } from './config';
-import logger from './logger';
+import logger from './utils/logger';
 
 class Database {
   private static instance: Database;
